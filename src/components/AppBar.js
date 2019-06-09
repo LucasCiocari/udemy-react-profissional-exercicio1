c039e806d6847984aaca44de2c5c6275eea2f622
Lucas Bertoglio Ciocari <lucas.ciocari@acad.pucrs.br>
2019-06-09 15:37:46 -0300

    advancing classes

---
+++ src/components/AppBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AppBar = ({onReload, onSave}) => (
+    <div className="app-bar">
+        <div className="app-bar__container">
+            <span className="app-bar__brand">Note JS</span>
+            <button className="app-bar__action" onClick={onReload}>
+                <i className="material-icons">refresh</i>
+            </button>
+            <button className="app-bar__action" onClick={onSave}>
+                <i className="material-icons">save</i>
+            </button>
+        </div>
+    </div>);
+
+export default AppBar;
